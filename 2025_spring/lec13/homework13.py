70f1c1f2def952175663df2077a317491c9866db
De Zoysa Yasitha Nimantha <yasinimantha@gmail.com>
2025-07-29 09:49:08 +0530

    homework13
    M24W7461

--- 2025_spring/lec13/homework13.py
+++ 2025_spring/lec13/homework13.py
@@ -12,8 +12,21 @@ def extract_stories_from_NPR_text(text):
       Each story should be a tuple of (title, teaser), where the title and teaser are
       both strings.  If the story has no teaser, its teaser should be an empty string.
     '''
-    raise RuntimeError('You need to write this part!')
+    #raise RuntimeError('You need to write this part!')
+    soup = bs4.BeautifulSoup(text, "html.parser")
+    div_tags = soup.find_all('div', 'story-text')
+    stories = []
+    for div_tag in div_tags:
+        title_tag = div_tag.find('h3', 'title')
+        teaser_tag = div_tag.find('p', 'teaser')
+
+        title = title_tag.get_text(strip=True) if title_tag else ''
+        teaser = teaser_tag.get_text(strip=True) if teaser_tag else ''
+
+        stories.append((title, teaser))
+
     return stories
+
     
 def read_nth_story(stories, n, filename):
     '''
@@ -26,4 +39,5 @@ def read_nth_story(stories, n, filename):
 
     Output: None
     '''
-    raise RuntimeError('You need to write this part!')
+    #raise RuntimeError('You need to write this part!')
+    gtts.gTTS(text=stories[n][0]+" "+stories[n][1], lang="en").save(filename)
